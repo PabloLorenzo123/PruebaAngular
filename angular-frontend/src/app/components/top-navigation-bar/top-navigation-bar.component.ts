a159dd8fbf05129d1d4d397ad21d142a37225dbb
Pablo Lorenzo <pabloernesto802@gmail.com>
2025-02-22 22:39:24 -0400

    Implemented navbar structure.

--- angular-frontend/src/app/components/top-navigation-bar/top-navigation-bar.component.ts
+++ angular-frontend/src/app/components/top-navigation-bar/top-navigation-bar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'top-navigation-bar',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './top-navigation-bar.component.html',
   styleUrl: './top-navigation-bar.component.scss'
 })
