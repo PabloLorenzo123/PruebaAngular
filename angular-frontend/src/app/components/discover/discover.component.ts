e1007c22acb6b08bf524fd7905f948af1ece8c16
Pablo Lorenzo <pabloernesto802@gmail.com>
2025-02-25 12:38:11 -0400

    Mvp working.

--- angular-frontend/src/app/components/discover/discover.component.ts
+++ angular-frontend/src/app/components/discover/discover.component.ts
@@ -2,31 +2,43 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { DiscoverSectionService } from '../../services/discover-section.service';
 import { discoverArticle } from '../../model/discoverArticle.type';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconService } from '../../services/mat-icon-service.service';
+
 import { catchError } from 'rxjs';
 
 @Component({
   selector: 'discover-section',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './discover.component.html',
   styleUrl: './discover.component.scss'
 })
 export class DiscoverComponent implements OnInit{
   discoverSectionService = inject(DiscoverSectionService);
   articles = signal<discoverArticle[]>([]);
+  selectedDisplayOption = signal<string>('more-icon');
 
-  
-  displayOptions =[
-    {svgSrc: '/svg/more.svg', className: 'more'},
-    {svgSrc: '/svg/grid_view.svg', className: 'grid'},
-    {svgSrc: '/svg/circle.svg', className: 'circle'}
-  ]
-  selectedDisplayOption = signal('more');
+  displayOptionsIcons = {
+    'more-icon': '/icons/more.svg',
+    'grid-icon': '/icons/grid_view.svg',
+    'circle-icon': '/icons/circle.svg',
+  }
 
-  selectDisplayOption(className: string): void {
-    this.selectedDisplayOption.set(className);
+  constructor(private matIconService: MatIconService){
+    this.matIconService.registerIcons(this.displayOptionsIcons);
+    this.selectedDisplayOption.set(Object.keys(this.displayOptionsIcons)[0]);
+  }
+
+  selectDisplayOption(icon: string): void {
+    this.selectedDisplayOption.set(icon);
+  }
+
+  getOptionsNames(){
+    return Object.keys(this.displayOptionsIcons);
   }
 
   ngOnInit(): void {
+    // Fetch the articles for this section.
       this.discoverSectionService.getArticles().pipe(
         catchError((err) => {
           throw err;
