d12e0dcf981e59e1ec8ce968ee508d024bc25a00
Pablo Lorenzo <pabloernesto802@gmail.com>
2025-02-25 12:52:07 -0400

    Only profile photo needs styling.

--- angular-frontend/src/app/components/navbar/navbar.component.ts
+++ angular-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,35 @@
 import { Component, signal } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AuthService } from '../../services/auth.service';
+import { MatIconService } from '../../services/mat-icon-service.service';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'navbar',
-  imports: [NgClass],
+  imports: [NgClass, MatIconModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   isMenuOpen = signal<boolean>(false);
 
-  toggleMenu() {
-    this.isMenuOpen.update(prev => !prev);
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private matIconService: MatIconService,
+  ) {
+    this.matIconService.registerIcon('navbar-icon', '/icons/navbar.svg');
   }
 
+  toggleMenu() {
+    this.isMenuOpen.update((prev) => !prev);
+  }
+
+  handleLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
